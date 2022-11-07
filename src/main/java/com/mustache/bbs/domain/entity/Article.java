b4b9850f4ee084dfe9f8c2eec30db44f7a275696
Sangjoon <djm04212@naver.com>
2022-11-07 15:29:34 +0900

    @Entity - @Id - @GeneratedValue 추가 +) @NoArgsConstructor

---
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -0,0 +1,23 @@
+package com.mustache.bbs.domain.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
