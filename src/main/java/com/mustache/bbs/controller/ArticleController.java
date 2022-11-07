ac0e930d35abf214f973af3e47eae26126a966df
Sangjoon <djm04212@naver.com>
2022-11-07 17:05:42 +0900

    [수정] articleRepository.save()

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
@@ -22,6 +29,7 @@ public class ArticleController {
     public String createArticle(ArticleDto form) {
         log.info(form.toString());
         Article article = form.toEntity();
+        articleRepository.save(article);
         return "";
     }
 
